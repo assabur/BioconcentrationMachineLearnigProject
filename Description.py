ef5f060ddead68d7ee97ef4e418f90e1d1b19769
assabur <maigaabdalahmohamed@gmail.com>
2020-11-28 20:32:36 +0100

    boxplot

--- Description.py
+++ Description.py
@@ -11,7 +11,9 @@ mask = np.triu(np.ones_like(corrMatrix, dtype=bool))
 # Set up the matplotlib figure
 f, ax = plt.subplots(figsize=(11, 15))
 # Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(corrMatrix,mask=mask, vmax=1, center=0,
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+sns.heatmap(corrMatrix,mask=mask,cmap= cmap, vmax=1, center=0,
             square=True,linewidths=.5, cbar_kws={"shrink": .5})
 plt.title("Correlation matrix QSAR fish bioconcentration factor ")
 plt.show()
